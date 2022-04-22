02626960e853881dcc0380a0f60e6175f8b42445
Machulskii Nikita <machulskii.nr@phystech.edu>
2022-04-22 05:46:54 +0300

    Es were alles

---
+++ HW10/server/server.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <thread>
+
+
+
+void read_message(boost::asio::ip::tcp::socket& sock)
+{
+	while (true) {
+		boost::asio::streambuf buffer;
+		std::string message;
+		boost::asio::read_until(sock, buffer, ';');
+		std::istream input_stream(&buffer);
+		getline(input_stream, message, ';');
+		std::cout << message << std::endl;
+	}
+}
+
+void write_message(boost::asio::ip::tcp::socket& sock, const std::string& name)
+{
+	std::string message;
+	std::cin >> message;
+	std::string output_message;
+
+	while (message != "QUIT")
+	{
+		output_message = "(" + name + "):   " + message + ';';
+		auto buf = boost::asio::buffer(output_message);
+		boost::asio::write(sock, buf);
+		std::cin >> message;
+	}
+
+	output_message = "(" + name + ")" + " has left the chat";
+	auto buf = boost::asio::buffer(output_message);
+	boost::asio::write(sock, buf);
+}
+
+int main(int argc, char* argv[])
+{
+	boost::asio::io_service io_service;
+	boost::asio::ip::tcp::endpoint final_point(boost::asio::ip::address_v4::any(), 42);
+	std::string name = "Server: ";
+	boost::asio::ip::tcp::acceptor accptr(io_service, final_point.protocol());
+	accptr.bind(final_point);
+	accptr.listen(10);
+	boost::asio::ip::tcp::socket socket(io_service);
+	accptr.accept(socket);
+	std::thread reader = std::thread(read_message, ref(socket));
+	write_message(socket, cref(name));
+	reader.join();
+	return EXIT_SUCCESS;
+}
